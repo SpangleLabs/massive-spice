5cecf9cfe8169c660f135d994598295de329168d
dr-spangle <dr-spangle@dr-spangle.com>
2014-11-15 23:07:49 +0000

    get_page(String,String) method which will get results of a page while sending x-modhash header

--- src/Web.java
+++ src/Web.java
@@ -102,6 +102,47 @@ public class Web {
 		return url_response.toString();
 	}
 	
+	public static String get_page(String url, String modhash) {
+		/* *
+		 * Downloads a page's contents, sending modhash header, without sending any data via POST.
+		 */
+		StringBuffer url_response = new StringBuffer();
+		BufferedReader url_in = null;
+		try {
+			URL url_obj = new URL(url);
+			HttpURLConnection url_con = (HttpURLConnection) url_obj.openConnection();
+			url_con.setRequestMethod("GET");
+			url_con.setRequestProperty("User-Agent:",user_agent);
+			url_con.setRequestProperty("X-Modhash:",modhash);
+			url_con.setDoOutput(true);
+			int url_response_code = url_con.getResponseCode();
+			System.out.println(url_response_code);
+			if(url_response_code==200) {
+				url_in = new BufferedReader(new InputStreamReader(url_con.getInputStream()));
+			} else {
+				url_in = new BufferedReader(new InputStreamReader(url_con.getErrorStream()));
+			}
+			String line;
+			while((line = url_in.readLine())!=null) {
+				url_response.append(line);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				url_in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(url_response.toString());
+		return url_response.toString();
+	}
+	
 	public static String build_query_string(HashMap<String,String> data, String type) {
 		/* *
 		 * Collapses hashmaps of the form {value:data} down to query strings for web requests.
