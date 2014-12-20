b6cb6e90b994a07af321f5f41595a48f80eac800
dr-spangle <dr-spangle@dr-spangle.com>
2014-12-20 19:49:58 +0000

    Started work on Giveaway object

---
+++ src/Giveaway.java
@@ -0,0 +1,48 @@
+
+public class Giveaway {
+	private String title = null;
+	private String message = null;
+	private String link = null;
+	
+	public String get_title() {
+		/* *
+		 * Returns the giveaway title.
+		 */
+		return this.title;
+	}
+	
+	public void set_title(String input_title) {
+		/* *
+		 * Sets the giveaway title.
+		 */
+		this.title = input_title;
+	}
+	
+	public String get_message() {
+		/* *
+		 * Returns the giveaway message.
+		 */
+		return this.message;
+	}
+	
+	public void set_message(String input_message) {
+		/* *
+		 * Sets the giveaway message.
+		 */
+		this.message = input_message;
+	}
+	
+	public String get_link() {
+		/* *
+		 * Returns the reddit giveaway link.
+		 */
+		return this.link;
+	}
+	
+	public void set_link(String input_link) {
+		/* *
+		 * Sets the reddit giveaway link.
+		 */
+		this.link = input_link;
+	}
+}
