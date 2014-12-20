86b16bcfb2df57fcc482f51473457f485d7625ae
dr-spangle <dr-spangle@dr-spangle.com>
2014-12-20 23:20:33 +0000

    Framework for a pokemon object made

---
+++ src/Pokemon.java
@@ -0,0 +1,21 @@
+
+public class Pokemon {
+	private String species = null;
+	private Integer coord_box = null;
+	private Integer coord_row = null;
+	private Integer coord_col = null;
+	private Integer iv_hp = null;
+	private Integer iv_atk = null;
+	private Integer iv_def = null;
+	private Integer iv_spa = null;
+	private Integer iv_spd = null;
+	private Integer iv_spe = null;
+	private String move_1 = null;
+	private String move_2 = null;
+	private String move_3 = null;
+	private String move_4 = null;
+	private String pokeball = null;
+	private String ESV = null;
+	private String ability = null;
+	private String gender = null;
+}
