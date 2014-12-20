ea1c1b836073981a0160635f590e8d24581c7d63
dr-spangle <dr-spangle@dr-spangle.com>
2014-12-20 23:22:18 +0000

    Added GiveawayPokemon framework class

---
+++ src/GiveawayPokemon.java
@@ -0,0 +1,7 @@
+
+public class GiveawayPokemon extends Pokemon {
+	private boolean giveaway_reserved = null;
+	private boolean giveaway_taken = null;
+	private String giveaway_player = null;
+	private String giveaway_gift = null;
+}
