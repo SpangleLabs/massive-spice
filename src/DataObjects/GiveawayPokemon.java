4db8c64ad4d30af93f2680ea24cccda97fdc808e
dr-spangle <dr-spangle@dr-spangle.com>
2014-12-22 00:39:38 +0000

    Added getters and setters

--- src/DataObjects/GiveawayPokemon.java
+++ src/DataObjects/GiveawayPokemon.java
@@ -6,4 +6,28 @@ public class GiveawayPokemon extends Pokemon {
 	private Boolean giveaway_taken = null;
 	private String giveaway_player = null;
 	private String giveaway_gift = null;
+	public Boolean getGiveaway_reserved() {
+		return giveaway_reserved;
+	}
+	public void setGiveaway_reserved(Boolean giveaway_reserved) {
+		this.giveaway_reserved = giveaway_reserved;
+	}
+	public Boolean getGiveaway_taken() {
+		return giveaway_taken;
+	}
+	public void setGiveaway_taken(Boolean giveaway_taken) {
+		this.giveaway_taken = giveaway_taken;
+	}
+	public String getGiveaway_player() {
+		return giveaway_player;
+	}
+	public void setGiveaway_player(String giveaway_player) {
+		this.giveaway_player = giveaway_player;
+	}
+	public String getGiveaway_gift() {
+		return giveaway_gift;
+	}
+	public void setGiveaway_gift(String giveaway_gift) {
+		this.giveaway_gift = giveaway_gift;
+	}
 }
