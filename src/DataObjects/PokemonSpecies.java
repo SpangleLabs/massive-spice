3d3ea0180d2a5a2de349e07e539fe5a98d930dc3
dr-spangle <dr-spangle@dr-spangle.com>
2014-12-23 11:44:13 +0000

    Added getters and setters to PokemonSpecies object

--- src/DataObjects/PokemonSpecies.java
+++ src/DataObjects/PokemonSpecies.java
@@ -6,4 +6,66 @@ public class PokemonSpecies {
 	private String Type1 = null;
 	private String Type2 = null;
 	private String EggCycles = null;
+	
+	public String getSpeciesName() {
+		/* *
+		 * Returns the species name.
+		 */
+		return SpeciesName;
+	}
+	public void setSpeciesName(String speciesName) {
+		/* *
+		 * Sets species name
+		 */
+		SpeciesName = speciesName;
+	}
+	public Integer getDexNational() {
+		/* *
+		 * Returns national pokedex number
+		 */
+		return DexNational;
+	}
+	public void setDexNational(Integer dexNational) {
+		/* *
+		 * Sets the national pokedex number.
+		 */
+		DexNational = dexNational;
+	}
+	public String getType1() {
+		/* *
+		 * Returns the pokemon's primary type.
+		 */
+		return Type1;
+	}
+	public void setType1(String type1) {
+		/* *
+		 * Sets the pokemon's primary type.
+		 */
+		Type1 = type1;
+	}
+	public String getType2() {
+		/* *
+		 * Returns the pokemon's secondary type or null.
+		 */
+		return Type2;
+	}
+	public void setType2(String type2) {
+		/* *
+		 * Sets the pokemon's secondary type.
+		 */
+		Type2 = type2;
+	}
+	public String getEggCycles() {
+		/* *
+		 * Returns the number of egg cycles it takes to hatch an egg.
+		 * One cycle is 255 steps.
+		 */
+		return EggCycles;
+	}
+	public void setEggCycles(String eggCycles) {
+		/* *
+		 * Sets the number of egg cycles required to hatch an egg.
+		 */
+		EggCycles = eggCycles;
+	}
 }
