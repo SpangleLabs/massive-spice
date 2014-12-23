6c8b7689488994a67bcffb7ebd64a0433c798042
dr-spangle <dr-spangle@dr-spangle.com>
2014-12-23 11:36:32 +0000

    Splitting full PokemonSpecies object off to PokemonSpeciesFull. Simplifying PokemonSpecies object to what it needs to be.

--- src/DataObjects/PokemonSpecies.java
+++ src/DataObjects/PokemonSpecies.java
@@ -2,33 +2,8 @@ package DataObjects;
 
 public class PokemonSpecies {
 	private String SpeciesName = null;
-	private String SpeciesDesc = null;
-	private String SpeciesJapanese = null;
-	private String SpeciesTransliterated = null;
 	private Integer DexNational = null;
-	private Integer DexJohto = null;
-	private Integer DexJohtoOld = null;
-	private Integer DexHoenn = null;
-	private Integer DexSinnoh = null;
-	private Integer DexUnova = null;
-	private Integer DexUnova2 = null;
-	private Integer DexKalos = null;
-	private String DexKalosArea = null;
-	private String DexFiore = null;
-	private String DexAlmia = null;
-	private String DexObliviaPresent = null;
-	private String DexObliviaPast = null;
-	private String Ability1 = null;
-	private String Ability2 = null;
-	private String AbilityHidden = null;
 	private String Type1 = null;
 	private String Type2 = null;
-	private String Catchrate = null;
-	private String EggGroup = null;
 	private String EggCycles = null;
-	private String HeightImp = null;
-	private String HeightMet = null;
-	private String WeightImp = null;
-	private String WeightMet = null;
-	private String LevellingType = null;
 }
