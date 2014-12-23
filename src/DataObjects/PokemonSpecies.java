a702c552442ad13faf4d927c850a412dd0aba6c7
dr-spangle <dr-spangle@dr-spangle.com>
2014-12-23 18:44:59 +0000

    Added saveToXML method to save the PokeSpecies object to XML

--- src/DataObjects/PokemonSpecies.java
+++ src/DataObjects/PokemonSpecies.java
@@ -1,7 +1,21 @@
 package DataObjects;
 
+import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 
 import Web.Web;
 
@@ -35,6 +49,53 @@ public class PokemonSpecies {
 		Pattern EggCyclesPattern = Pattern.compile("\\Weggcycles=([^ |]*)");
 		Matcher EggCyclesMatcher = EggCyclesPattern.matcher(code);
 		this.EggCycles = Integer.parseInt(EggCyclesMatcher.group(1).trim());
+	}
+	
+	public void saveToXML() {
+		/* *
+		 * Saves pokemon species information to XML.
+		 */
+		try {
+			DocumentBuilderFactory DocFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder DocBuilder = DocFactory.newDocumentBuilder();
+			//build root element
+			Document Doc = DocBuilder.newDocument();
+			Element RootElement = Doc.createElement("Pokemon");
+			Doc.appendChild(RootElement);
+			//add Name element
+			Element NameElement = Doc.createElement("SpeciesName");
+			NameElement.appendChild(Doc.createTextNode(this.SpeciesName));
+			RootElement.appendChild(NameElement);
+			//add national dex number
+			Element DexElement = Doc.createElement("DexNational");
+			DexElement.appendChild(Doc.createTextNode(this.DexNational.toString()));
+			RootElement.appendChild(DexElement);
+			//add type1
+			Element Type1Element = Doc.createElement("Type1");
+			Type1Element.appendChild(Doc.createTextNode(this.Type1));
+			RootElement.appendChild(Type1Element);
+			//add type2?
+			Element Type2Element = Doc.createElement("Type2");
+			Type2Element.appendChild(Doc.createTextNode(this.Type2));
+			RootElement.appendChild(Type2Element);
+			//add egg cycles
+			Element EggElement = Doc.createElement("EggCycles");
+			EggElement.appendChild(Doc.createTextNode(this.EggCycles.toString()));
+			RootElement.appendChild(EggElement);
+			//save the xml to file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(Doc);
+			File dir = new File("config/pokemon");
+			dir.mkdirs();
+			String filename = "config/pokemon/"+String.format("%04d",this.DexNational)+".xml";
+			StreamResult result = new StreamResult(new File(filename));
+			transformer.transform(source, result);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getSpeciesName() {
